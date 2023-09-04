c2b3e16acb81ea9f96a24e18fa596e65437f7276
Jose Muñoz <josemunoz@Joses-MacBook-Pro.local>
2023-09-04 12:28:39 -0500

    initial commit

---
+++ src/test/java/com/juniorjl83/webflux/TestReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorjl83.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
